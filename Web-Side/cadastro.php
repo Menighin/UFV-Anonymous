1658d9cc618c214cd2a67484e5662c53a9e4042f
Thiago Silva <thiagodd.silva@gmail.com>
2013-11-03 10:02:28 -0200

    Conexão com o banco de dados e load da lista de faculdades cadastradas.

--- Web-Side/cadastro.php
+++ Web-Side/cadastro.php
@@ -1,39 +1,52 @@
+<?php 
+    include_once('database.class.php');
+    
+    $database = new Database();
+    $database->connect();
+?>
 <html>
-	<head>
-		<meta charset="utf-8" />
-		<title> Home Page </title>
-	</head>
-	
-	<body>
-		<form id="form" action="" method="POST">
-			<label for="username">Nome de Usuário: </label>
-			<input type="text" name="username" id="username" />
-			<br />
-			
-			<label for="password">Senha: </label>
-			<input type="password" name="password" id="password" />
-			<br />
-			
-			<label for="password-confirm">Confirmação da Senha: </label>
-			<input type="password" name="password-confirm" id="password-confirm" />
-			<br />
-			
-			<label for="email">Email: </label>
-			<input type="text" name="email" id="email" />
-			<br />
-			
-			<label for="university">Faculdade: </label>
-			<select>	
-				<option value="">Selecione...</option>
-				<!-- Carregar a lista de faculdades -->
-			</select>
-			<br />
-			
-			<label for="sex">Sexo: </label>
-			<select>
-				<option value="M">Homem</option>
-				<option value="F">Mulher</option>
-			</select>
-		</form>
-	</body>
+    <head>
+        <meta charset="utf-8" />
+        <title> Home Page </title>
+    </head>
+
+    <body>
+        <form id="form" action="" method="POST">
+            <label for="username">Nome de Usuário: </label>
+            <input type="text" name="username" id="username" />
+            <br />
+
+            <label for="password">Senha: </label>
+            <input type="password" name="password" id="password" />
+            <br />
+
+            <label for="password-confirm">Confirmação da Senha: </label>
+            <input type="password" name="password-confirm" id="password-confirm" />
+            <br />
+
+            <label for="email">Email: </label>
+            <input type="text" name="email" id="email" />
+            <br />
+
+            <label for="university">Faculdade: </label>
+            <select>
+                <option value="-1">Selecione...</option>
+                <!-- Carregar a lista de faculdades -->
+                <?php
+                    $rs = $database->getUniversities();
+                    /* Cria um option para cada registro no banco. */
+                    foreach($rs as $registro) {
+                        echo '<option value="' . $registro['id'] . '">' . $registro['name'] . '</option>';
+                    }
+                ?>
+            </select>
+            <br />
+
+            <label for="sex">Sexo: </label>
+            <select>
+                <option value="M">Homem</option>
+                <option value="F">Mulher</option>
+            </select>
+        </form>
+    </body>
 </html>
---
+++ Web-Side/database.class.php
@@ -0,0 +1,44 @@
+<?php
+
+class Database {
+
+    private $conn = null;
+    private $username = 'california';
+    private $password = '123456';
+    private $host = 'localhost';
+    private $dbname = 'anonymouschat';
+
+    /* Abre uma conexão com o banco de dados. */
+
+    function connect() {
+        try {
+            //$this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->dbname, 
+             //                       $this->username, $this->password);
+            
+            $this->conn = new PDO("mysql:host=localhost;dbname=anonymouschat", "root", "california");
+            
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->conn->exec('SET CHARACTER SET utf8');
+            $this->conn->exec('SET NAMES utf8');
+            
+        } catch (PDOException $ex) {
+            echo 'Erro ao se conectar com o banco de dados.';
+            echo $ex;
+        }
+
+        return $this->conn;
+    }
+
+    function disconnect() {
+        $this->conn = null;
+        return $this->conn;
+    }
+    
+    /* Retorna a lista de faculdades gravada no banco de dados. */
+    function getUniversities() {
+        $resultSet = $this->conn->query("SELECT * FROM universities")->fetchall();
+        return $resultSet;
+    }
+}
+
+?>
