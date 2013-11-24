1bd6708d8c1901af773698b14709762d890262e6
João Menighin <joao.menighin@gmail.com>
2013-11-23 22:29:46 -0200

    Start website

---
+++ Web-Side/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>UniChat</title>
+		<link rel="stylesheet" href="css/style.css">
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+	</head>
+	<body>
+		<div id="wrapper">
+			<header>
+				<div id="logo">
+					<img src="img/logo.png" />
+				</div>
+				<nav id="menu">
+					<a href="Home">Home</a> |
+					<a href="Estatistica">Estatisticas</a> |
+					<a href="Reportar">Reportar bugs</a>
+				</nav>
+				<nav id="login">
+					<a href="Login"> Login </a> |
+					<a href="Registrar"> Register </a>
+				</nav>
+			<header>
+			<section id="content">
+				<p> CONTENT </p>
+			</section>
+			<footer>
+				<p> FOOTER </p>
+			</footer>
+		</div>
+	</body>
+</html>
