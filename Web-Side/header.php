ece58ee7c7e850325818cc84157dbc3b70709664
João Menighin <joao.menighin@gmail.com>
2014-01-21 23:51:01 -0200

    Site responsive para mobile...?
    Testar se fico xuxu beleza

--- Web-Side/header.php
+++ Web-Side/header.php
@@ -16,39 +16,39 @@
 	<body>
 		<div id="wrapper">
 			<header>
+				<div id="login">
+					<?php
+						if (!isset($_SESSION['user'])) {
+					?>
+					
+					<form>
+						<input type="text" id="userLogin" placeholder="Usuário" />
+						<input type="password" id="passwordLogin" placeholder="Senha" />
+					</form>
+					<a class="button" onclick="login();"> Login </a>
+					<a class="button" href="register.php"> Registrar </a>
+					<div id="loginMsg"></div>
+					<div id="forgotPassword"><a href="/forgot.php">Esqueci minha senha</a></div>
+					<div id="loadingLogin"><img src="img/loader.gif" width="25" height="25" /></div>
+					<?php
+						} else {
+					?>
+					<div id="logged">
+						Bem-vindo <span><?php echo $_SESSION['user']; ?></span><br />
+						<a href="/profile.php">Painel de controle</a> | <a href="/logout.php">Logout</a>
+					</div>
+					<?php
+						}
+					?>
+				</div>
 				<div id="logo">
 					<a href="http://www.unichat.com.br"><img src="img/logo.png" /></a>
 				</div>
 				<div id="menuContent">
-					<div id="login">
-						<?php
-							if (!isset($_SESSION['user'])) {
-						?>
-						
-						<form>
-							<input type="text" id="userLogin" placeholder="Usuário" /><br />
-							<input type="password" id="passwordLogin" placeholder="Senha" />
-						</form>
-						<a class="button" onclick="login();"> Login </a>
-						<a class="button" href="register.php"> Registrar </a>
-						<div id="loginMsg"></div>
-						<div id="forgotPassword"><a href="/forgot.php">Esqueci minha senha</a></div>
-						<div id="loadingLogin"><img src="img/loader.gif" width="25" height="25" /></div>
-						<?php
-							} else {
-						?>
-						<div id="logged">
-							Bem-vindo <span><?php echo $_SESSION['user']; ?></span><br />
-							<a href="/profile.php">Painel de controle</a> | <a href="/logout.php">Logout</a>
-						</div>
-						<?php
-							}
-						?>
-					</div>
 					<div id="menuWrapper">
 						 <img id="menutip1" src="img/menutip1.png" /> 
 						<div id="menu">
-							<div class="menuItem" href="http://www.unichat.com.br">Home</div>
+							<a class="menuItem" href="http://www.unichat.com.br">Home</a>
 							<a class="menuItem" href="http://www.unichat.com.br">Estatísticas</a>
 							<a class="menuItem" href="http://www.unichat.com.br">Reportar Bugs</a>
 						</div>
@@ -63,10 +63,11 @@
 							 data-ad-slot="6807286609"></ins>
 						<script>
 							(adsbygoogle = window.adsbygoogle || []).push({});
-							</script>
+						</script>
+						<?php  include "analyticstracking.php"; ?>
 					</div>
 					
-					<?php  include "analyticstracking.php"; ?>
+					
 					
 				</div>
-			<header>
+			</header>
