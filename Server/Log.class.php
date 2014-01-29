94b6967e7a565dcbcef558934eb6180243d0e94b
João Menighin <joao.menighin@gmail.com>
2014-01-29 20:15:08 -0200

    Fix na classe do Log pra printar o \n

--- Server/Log.class.php
+++ Server/Log.class.php
@@ -4,7 +4,7 @@ class Log {
 	public static function writeLog($message) {
 		
 		$timestamp = "[" . date("H:i:s") . "] ";
-		file_put_contents ("logs/" . date("Y-m-d"). ".txt", $timestamp . $message, FILE_APPEND);
+		file_put_contents ("logs/" . date("Y-m-d"). ".txt", $timestamp . $message . "\n", FILE_APPEND);
 		
 	}
 }
