07ae0bf5494e160ac9cbd7f78a899942269183fa
Thiago Silva <thiagodd.silva@gmail.com>
2014-02-20 21:35:09 -0300

    Classes restantes da estrutura do GCM.

---
+++ Android/UniChat/src/br/com/unichat/activities/GcmBroadcastReceiver.java
@@ -0,0 +1,14 @@
+package br.com.unichat.activities;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.content.WakefulBroadcastReceiver;
+
+public class GcmBroadcastReceiver extends WakefulBroadcastReceiver {
+
+	@Override
+	public void onReceive(Context arg0, Intent arg1) {
+		
+		
+	}
+}
--- Android/UniChat/src/br/com/unichat/activities/GcmIntentService.java
+++ Android/UniChat/src/br/com/unichat/activities/GcmIntentService.java
@@ -2,6 +2,9 @@ package br.com.unichat.activities;
 
 import android.app.IntentService;
 import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.gms.gcm.GoogleCloudMessaging;
 
 public class GcmIntentService extends IntentService {
 
@@ -11,6 +14,18 @@ public class GcmIntentService extends IntentService {
 
 	@Override
 	protected void onHandleIntent(Intent intent) {
+		Bundle extras = intent.getExtras();
+		GoogleCloudMessaging googleCloud = GoogleCloudMessaging.getInstance(getApplicationContext());
+		String messageType = googleCloud.getMessageType(intent);
 		
+		if(!extras.isEmpty()) {
+			if(GoogleCloudMessaging.MESSAGE_TYPE_SEND_ERROR.equals(messageType)) {
+				
+			} else if(GoogleCloudMessaging.MESSAGE_TYPE_DELETED.equals(messageType)) {
+				
+			} else if(GoogleCloudMessaging.MESSAGE_TYPE_MESSAGE.equals(messageType)) {
+				
+			}
+		}
 	}
 }
